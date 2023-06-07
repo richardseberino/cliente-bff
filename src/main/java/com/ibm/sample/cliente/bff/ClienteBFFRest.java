88bca9fcbb4ec58d3d13979286f8daaed9caf182
richardseberino <31409511+richardseberino@users.noreply.github.com>
2023-06-07 11:44:19 -0300

    Ajuste no tratamento de erros

--- src/main/java/com/ibm/sample/cliente/bff/ClienteBFFRest.java
+++ src/main/java/com/ibm/sample/cliente/bff/ClienteBFFRest.java
@@ -146,7 +146,7 @@ public class ClienteBFFRest {
 			{
 				logger.warn("Customer requested to be deleted does not exist with this ID: " + cpf);
 				span.log("Customer requested to be deleted does not exist with this ID: " + cpf);
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+				return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 			}	
 			HttpHeaders httpHeaders = new HttpHeaders();
 			HttpHeaderInjectAdapter h1 = new HttpHeaderInjectAdapter(httpHeaders);
